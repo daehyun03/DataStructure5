a6f5c81246a1b08b483d0f2762f2618eeb0fbf29
DoDoGaMaRu <nm09090@gmail.com>
2022-05-25 18:06:38 +0900

    BottomUpMergeSort 개선

--- sortAlgorithm/src/daeHyun/BottomUpMergeSort.java
+++ sortAlgorithm/src/daeHyun/BottomUpMergeSort.java
@@ -1,7 +1,6 @@
 package daeHyun;
 
 public class BottomUpMergeSort {
-
     public static void sort(Comparable[] arr) {
         Comparable[] tempArr = new Comparable[arr.length];
         int high = arr.length - 1;
@@ -16,31 +15,23 @@ public class BottomUpMergeSort {
         int idx1 = low;
         int idx2 = mid + 1;
 
-        for (int i = low; i <= high; i++) {
-            if (idx1 > mid) {
-                for (int j = idx2; j <= high; j++) {
-                    tempArr[i++] = arr[j];
-                }
-            }
-            else if (idx2 > high) {
-                for (int j = idx1; j <= mid; j++) {
-                    tempArr[i++] = arr[j];
-                }
-            }
-            else if (isLess(arr[idx2], arr[idx1])) {
-                tempArr[i] = arr[idx2++];
-            }
-            else {
-                tempArr[i] = arr[idx1++];
-            }
+        int tempIdx = low;
+        while (idx1 <= mid && idx2 <= high) {
+            if (arr[idx1].compareTo(arr[idx2]) <= 0)
+                tempArr[tempIdx++] = arr[idx1++];
+            else
+                tempArr[tempIdx++] = arr[idx2++];
+        }
+
+        while (idx1 <= mid) {
+            tempArr[tempIdx++] = arr[idx1++];
+        }
+        while (idx2 <= high) {
+            tempArr[tempIdx++] = arr[idx2++];
         }
 
         for (int i = low; i <= high; i++) {
             arr[i] = tempArr[i];
         }
-    }
-
-    private static boolean isLess(Comparable e1, Comparable e2) {
-        return e1.compareTo(e2) < 0;
     }
 }
