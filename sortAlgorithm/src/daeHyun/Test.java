82eb256decb4d60c7e050872af21600e09665b7f
daehyun <kimdeahyeon5445@gmail.com>
2022-05-25 14:56:17 +0900

    test문 수정

--- sortAlgorithm/src/daeHyun/Test.java
+++ sortAlgorithm/src/daeHyun/Test.java
@@ -1,15 +1,21 @@
 package daeHyun;
 
+import daeHwan.HeapSort;
+import daeHwan.RecursiveMergeSort;
 import main.RandomArrayCreator;
+import minJun.InsertionSort;
 
 import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        Integer[] arr = RandomArrayCreator.intArr(50);
-        System.out.println("before : " + Arrays.toString(arr));
-
-        BubbleSort.sort(arr);
-        System.out.print("after : " + Arrays.toString(arr));
+        for ( int i = 1; i < 10001; i = i*10) {
+            Integer[] arr = RandomArrayCreator.intArr(i);
+            long beforeTime = System.nanoTime();
+            RecursiveMergeSort.sort(arr);
+            long afterTime = System.nanoTime();
+            double time = (double)(afterTime - beforeTime)/1000000;
+            System.out.println(i + " time : " + time);
+        }
     }
 }
