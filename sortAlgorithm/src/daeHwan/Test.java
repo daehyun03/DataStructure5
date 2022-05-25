321371c2a8fc6fb3174cb329edf9c67b74410f48
DoDoGaMaRu <nm09090@gmail.com>
2022-05-25 16:51:43 +0900

    temp

--- sortAlgorithm/src/daeHwan/Test.java
+++ sortAlgorithm/src/daeHwan/Test.java
@@ -6,13 +6,75 @@ import main.Student;
 import java.util.Arrays;
 
 public class Test {
-    public static void main(String[] args) {
-/*        Integer[] arr = RandomArrayCreator.intArr(100);
-        HeapSort.sort(arr);
+    public static void main(String[] args) throws Exception{
+        Integer[] arr1 = RandomArrayCreator.intArr(1000000);
+        Integer[] arr2 = arr1.clone();
+        Integer[] arr3 = arr1.clone();
 
-        System.out.print(Arrays.toString(arr));*/
+        long time1 = System.currentTimeMillis();
+        Thread.sleep(3000);
+        RecursiveMergeSort.sort(arr1);
+        long time1end = System.currentTimeMillis();
 
-        Student a = new Student("홍길동", 10, 20, 32, 45, 51);
-        System.out.print(a.getAvg());
+        long t1 = time1end - time1 - 3000;
+
+
+        long time2 = System.currentTimeMillis();
+        Thread.sleep(3000);
+        HeapSort.sort(arr2);
+        long time2end = System.currentTimeMillis();
+
+        long t2 = time2end - time2 - 3000;
+
+
+        long time3 = System.currentTimeMillis();
+        Thread.sleep(3000);
+        Arrays.sort(arr3);
+        long time3end = System.currentTimeMillis();
+
+        long t3 = time3end - time3 - 3000;
+
+        System.out.println(t1 + " : " + t2 + " : " + t3);
     }
+/*
+    public static void nonSleep(int size) throws Exception {
+        Integer[] arr1 = RandomArrayCreator.intArr(size);
+        Integer[] arr2 = arr1.clone();
+
+        long time1 = System.currentTimeMillis();
+        HeapSort.sort(arr1);
+        long time1end = System.currentTimeMillis();
+
+        long t1 = time1end - time1;
+
+        long time2 = System.currentTimeMillis();
+        HeapSort.sort(arr2);
+        long time2end = System.currentTimeMillis();
+
+        long t2 = time2end - time2;
+
+        System.out.println(t1 + " : " + t2);
+    }
+
+    public static void sleep(int size) throws Exception {
+        Integer[] arr1 = RandomArrayCreator.intArr(size);
+        Integer[] arr2 = arr1.clone();
+
+        long time1 = System.currentTimeMillis();
+        Thread.sleep(3000);
+        HeapSort.sort(arr1);
+        long time1end = System.currentTimeMillis();
+
+        long t1 = time1end - time1 - 3000;
+
+        long time2 = System.currentTimeMillis();
+        Thread.sleep(3000);
+        HeapSort.sort(arr2);
+        long time2end = System.currentTimeMillis();
+
+        long t2 = time2end - time2 - 3000;
+
+        System.out.println(t1 + " : " + t2);
+    }
+ */
 }
