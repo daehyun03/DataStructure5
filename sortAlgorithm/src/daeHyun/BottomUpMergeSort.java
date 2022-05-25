0c0597ab9d674108861d578ab5cec76dee32a740
daehyun <kimdeahyeon5445@gmail.com>
2022-05-25 17:12:19 +0900

    test문 수정

---
+++ sortAlgorithm/src/daeHyun/BottomUpMergeSort.java
@@ -0,0 +1,45 @@
+package daeHyun;
+
+public class BottomUpMergeSort {
+    public static void sort(Comparable[] arr) {
+        Comparable[] tempArr = new Comparable[arr.length];
+        int high = arr.length - 1;
+        for ( int size = 1; size < arr.length; size = size*2 ) {
+            for ( int low = 0; low < high; low = low + size*2 ) {
+                merge(arr, tempArr, low, Math.min(low+size-1,high), Math.min(low+size*2-1,high));
+            }
+        }
+    }
+
+    private static void merge(Comparable[] arr, Comparable[] tempArr, int low, int mid, int high) {
+        int idx1 = low;
+        int idx2 = mid + 1;
+
+        for (int i = low; i <= high; i++) {
+            if (idx1 > mid) {
+                for (int j = idx2; j <= high; j++) {
+                    tempArr[i++] = arr[j];
+                }
+            }
+            else if (idx2 > high) {
+                for (int j = idx1; j <= mid; j++) {
+                    tempArr[i++] = arr[j];
+                }
+            }
+            else if (isLess(arr[idx2], arr[idx1])) {
+                tempArr[i] = arr[idx2++];
+            }
+            else {
+                tempArr[i] = arr[idx1++];
+            }
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = tempArr[i];
+        }
+    }
+
+    private static boolean isLess(Comparable e1, Comparable e2) {
+        return e1.compareTo(e2) < 0;
+    }
+}
--- sortAlgorithm/src/daeHyun/Test.java
+++ sortAlgorithm/src/daeHyun/Test.java
@@ -5,17 +5,27 @@ import daeHwan.RecursiveMergeSort;
 import main.RandomArrayCreator;
 import minJun.InsertionSort;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        for ( int i = 1; i < 10001; i = i*10) {
+        // 시간비교
+       /* for ( int i = 1; i < 1000001; i = i*10) {
+       // for ( int i = 100000; i > 0; i = i/10) {
             Integer[] arr = RandomArrayCreator.intArr(i);
             long beforeTime = System.nanoTime();
-            RecursiveMergeSort.sort(arr);
+            BubbleSort.sort(arr);
             long afterTime = System.nanoTime();
             double time = (double)(afterTime - beforeTime)/1000000;
-            System.out.println(i + " time : " + time);
-        }
+            BigDecimal time2 = new BigDecimal(time);
+            System.out.println(i + " time : " + time2);
+        }*/
+
+        //정렬확인
+        Integer[] arr = RandomArrayCreator.intArr(100);
+        BottomUpMergeSort.sort(arr);
+        System.out.print(Arrays.toString(arr) + "\n");
     }
+
 }
