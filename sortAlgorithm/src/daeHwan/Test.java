856437ba2c9155d420f893d77b9206d1348bb5c1
DoDoGaMaRu <nm09090@gmail.com>
2022-05-25 17:14:59 +0900

    재귀합병정렬 속도이슈 해결

--- sortAlgorithm/src/daeHwan/Test.java
+++ sortAlgorithm/src/daeHwan/Test.java
@@ -1,5 +1,8 @@
 package daeHwan;
 
+import daeHyun.BubbleSort;
+import jongHyeon.MedianOfThreeQuickSort;
+import jongHyeon.RecursiveQuickSort;
 import main.RandomArrayCreator;
 import main.Student;
 
@@ -7,34 +10,49 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) throws Exception{
-        Integer[] arr1 = RandomArrayCreator.intArr(1000000);
+        Integer[] arr = RandomArrayCreator.intArr(10);
+        RecursiveMergeSort.sort(arr);
+
+        System.out.print("after : " + Arrays.toString(arr));
+
+        /*Integer[] arr1 = RandomArrayCreator.intArr(1000000);
         Integer[] arr2 = arr1.clone();
         Integer[] arr3 = arr1.clone();
+        Integer[] arr4 = arr1.clone();
+
 
         long time1 = System.currentTimeMillis();
-        Thread.sleep(3000);
+        Thread.sleep(300);
         RecursiveMergeSort.sort(arr1);
         long time1end = System.currentTimeMillis();
 
-        long t1 = time1end - time1 - 3000;
+        long t1 = time1end - time1 - 300;
 
 
         long time2 = System.currentTimeMillis();
-        Thread.sleep(3000);
-        HeapSort.sort(arr2);
+        Thread.sleep(300);
+        MedianOfThreeQuickSort.sort(arr2);
         long time2end = System.currentTimeMillis();
 
-        long t2 = time2end - time2 - 3000;
+        long t2 = time2end - time2 - 300;
 
 
         long time3 = System.currentTimeMillis();
-        Thread.sleep(3000);
+        Thread.sleep(300);
         Arrays.sort(arr3);
         long time3end = System.currentTimeMillis();
 
-        long t3 = time3end - time3 - 3000;
+        long t3 = time3end - time3 - 300;
 
-        System.out.println(t1 + " : " + t2 + " : " + t3);
+
+        long time4 = System.currentTimeMillis();
+        Thread.sleep(300);
+        RecursiveQuickSort.sort(arr4);
+        long time4end = System.currentTimeMillis();
+
+        long t4 = time4end - time4 - 300;
+
+        System.out.println(t1 + " : " + t2 + " : " + t3 + " : " + t4);*/
     }
 /*
     public static void nonSleep(int size) throws Exception {
