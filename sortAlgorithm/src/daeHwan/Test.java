0c17cacd947ae5a09ac16d90dfb46189d108dc5e
DoDoGaMaRu <nm09090@gmail.com>
2022-05-31 17:45:24 +0900

    자연머지소트

--- sortAlgorithm/src/daeHwan/Test.java
+++ sortAlgorithm/src/daeHwan/Test.java
@@ -1,80 +1,34 @@
 package daeHwan;
 
-import daeHyun.BottomUpMergeSort;
 import main.RandomArrayCreator;
 
 
 public class Test {
-    public static void main(String[] args) {
-        int size = 1000000;
+    public static void main(String[] args) throws InterruptedException {
+        int size = 100000;
 
-        int time = 50;
+        Integer[] arr1 = RandomArrayCreator.intArr(size);
+        Integer[] arr2 = arr1.clone();
+        Integer[] arr3 = arr1.clone();
+        Integer[] arr4 = arr1.clone();
 
-        long sumNMS = 0;
-        long sumBUMS = 0;
-        long sumMS = 0;
-        long nms;
-        long bums;
-        long ms;
-
-        System.out.printf("NMS \t: BUMS \t: MS \n");
-
-        for (int i = 0; i < time; i++) {
-            Integer[] arr1 = RandomArrayCreator.intArr(size);
-            Integer[] arr2 = arr1.clone();
-            Integer[] arr3 = arr1.clone();
-
-            ms = testMS(arr3);
-            bums = testBUMS(arr2);
-            nms = testNMS(arr1);
-
-            sumNMS += nms;
-            sumBUMS += bums;
-            sumMS += ms;
-
-            System.out.printf("%d \t: %d \t: %d \n", nms, bums, ms);
-        }
-        System.out.println("????");
-        System.out.printf("%f \t: %f \t: %f \n", (double)sumNMS / time, (double)sumBUMS / time, (double)sumMS / time);
-    }
+        RecursiveMergeSort.sort(arr3);
+        NaturalMergeSort2.sort(arr4);
 
 
-    public static long testNMS(Integer[] arr) {
-        long t1 = System.currentTimeMillis();
-        try {
-            Thread.sleep(500);
-            NaturalMergeSort.sort(arr);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        double startTime_rec2 = System.nanoTime();
+        NaturalMergeSort2.sort(arr2);
+        double finishTime_rec2 = System.nanoTime();
+        double elapsedTime_rec2 = finishTime_rec2 - startTime_rec2;
+        System.out.println(elapsedTime_rec2 / 1000000.0);
 
-        return System.currentTimeMillis() - t1 - 500;
-    }
 
-    public static long testBUMS(Integer[] arr) {
-        long t1 = System.currentTimeMillis();
-        try {
-            Thread.sleep(500);
-            BottomUpMergeSort.sort(arr);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        double startTime_rec = System.nanoTime();
+        RecursiveMergeSort.sort(arr1);
+        double finishTime_rec = System.nanoTime();
+        double elapsedTime_rec = finishTime_rec - startTime_rec;
+        System.out.println(elapsedTime_rec / 1000000.0);
 
-        return System.currentTimeMillis() - t1 - 500;
-    }
 
-    public static long testMS(Integer[] arr) {
-        long t1 = System.currentTimeMillis();
-        try {
-            Thread.sleep(500);
-            RecursiveMergeSort.sort(arr);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return System.currentTimeMillis() - t1 - 500;
     }
 }
